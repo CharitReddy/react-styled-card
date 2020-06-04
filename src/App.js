f1a15099c6d72373c48cba962054b2c90146b47e
Shubham <shubham.shrivastava@nineleaps.com>
2020-06-04 17:00:23 +0530

    Initial Commit
    Set up the project using create react app.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <Card cardTitle="card-title" className="small-card" >content</Card>
+     <Card cardTitle="card-title" className="small-card" >Card Content</Card>
     </div>
   );
 }
